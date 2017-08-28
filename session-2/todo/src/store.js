7d05a8d5ec31a65cd82fc15f3f30a3bfc6768013
Pooya Parsa <pooya@pi0.ir>
2017-08-28 11:38:36 +0430

    session-2

---
+++ session-2/todo/src/store.js
@@ -0,0 +1,25 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+// Install vuex for vue
+Vue.use(Vuex)
+
+// Create new Store
+const store = new Vuex.Store({
+    state: {
+        year: 2016
+    },
+    getters: {
+
+    },
+    mutations: {
+
+    },
+    actions: {
+
+    }
+})
+
+// module.exports = store
+export default store
+
